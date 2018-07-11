227fb0a522dbef8f5de20a898d07bdd4e8da5f05
Kira Hartness <kirahartness@gmail.com>
2018-07-11 19:03:02 +0000

    automatically backed up by learn

--- arrays.rb
+++ arrays.rb
@@ -29,7 +29,7 @@ cart_item_prices.each do |price|
   total += price
 end
 
-sandwiches = [["ham", "swiss"],["turkey", "cheddar"],["roast beef", "gruyere"]]
+sandwiches = [["ham", "swiss"],["turkey", "cheddar"],["roast beef", "gruyere"]].reverse
 sandwiches.each do |indsandwich|
   puts "#{indsandwich}"
 end
