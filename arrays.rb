9e4d0dfdc14811588ff6b09bb63ed21e1f20968a
Kira Hartness <kirahartness@gmail.com>
2018-07-11 18:47:23 +0000

    automatically backed up by learn

--- arrays.rb
+++ arrays.rb
@@ -5,6 +5,7 @@ numbers.each do |x|
   increase << new_number
 end
 puts increase
+
 tax_included = []
 cart_item_price = [12.50, 19.99, 3.49, 7.99]
 cart_item_price.each do |price|
@@ -12,3 +13,6 @@ cart_item_price.each do |price|
   tax_included << price_with_tax
 end
 puts tax_included
+
+big_ticket_prices = []
+cart_item_prices = [12.50, ]
